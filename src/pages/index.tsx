03e6627fa909ed4339d7b7ad0f03210411f1b293
Kingsley <kingsley@eneris.ca>
2025-04-14 14:22:34 -0700

    Modified features and icons. Add create account button and update styling on home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,12 +16,23 @@ function HomepageHeader() {
           {siteConfig.title}
         </Heading>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/overview">
-            Get Started!
-          </Link>
+        <div className={styles.homeButtonContainer}>
+          <div className={styles.buttons}>
+            <Link
+              className="button button--secondary button--lg"
+              to="/docs/overview">
+              See documentation
+            </Link>
+          </div>
+          <div className={styles.buttons}>
+            <Link
+              className="button button--outline button--lg heroButton"
+              to="https://ener.is/join?utm_source=docs"
+              target="_blank"
+              rel="noopener noreferrer">
+              Create account
+            </Link>
+          </div>
         </div>
       </div>
     </header>
